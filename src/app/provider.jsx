a1544763ab12658cf6eedb83c6e2dd0119af6dbd
CodingWithHardik <hardikgupta0002@gmail.com>
2024-08-24 22:12:34 +0530

    project setup with theme

---
+++ src/app/provider.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import {NextUIProvider} from '@nextui-org/react'
+
+export function Providers({children, ...props }) {
+  return (
+    <NextUIProvider>
+      <NextThemesProvider {...props}>
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  )
+}
