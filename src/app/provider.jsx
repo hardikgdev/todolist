891f0dcd55649dac3d2eeb0d535a7f23bf9a9b19
CodingWithHardik <hardikgupta0002@gmail.com>
2024-08-30 13:18:40 +0530

    Auth Setup

--- src/app/provider.jsx
+++ src/app/provider.jsx
@@ -4,31 +4,34 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { NextUIProvider } from '@nextui-org/react';
 import DashboardLayout from "@/components/blocks/DashboardLayout";
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
 import DeviceNotSup from "@/components/blocks/DeviceNotSup";
+import { SessionProvider } from "next-auth/react";
 
-export function Providers({ children, ...props }) {
+export function Providers({ children, session, ...props }) {
   const pathname = usePathname();
   const navPaths = ['/', '/todo'];
 
   const showNavbar = navPaths.includes(pathname);
 
   return (
-    <NextUIProvider>
-      <NextThemesProvider {...props}>
-        <div className="md:hidden">
-          <DeviceNotSup />
-        </div>
-        <div className="hidden md:block">
-          {showNavbar ? (
-            <DashboardLayout>
-              {children}
-            </DashboardLayout>
-          ) : (
-            children
-          )}
-        </div>
-      </NextThemesProvider>
-    </NextUIProvider>
+    <SessionProvider session={session}>
+      <NextUIProvider>
+        <NextThemesProvider {...props}>
+          <div className="md:hidden">
+            <DeviceNotSup />
+          </div>
+          <div className="hidden md:block">
+            {showNavbar ? (
+              <DashboardLayout>
+                {children}
+              </DashboardLayout>
+            ) : (
+              children
+            )}
+          </div>
+        </NextThemesProvider>
+      </NextUIProvider>
+    </SessionProvider>
   );
 }
